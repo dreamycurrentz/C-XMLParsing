1978bd2bbb6504f1ee2171dad900f37e07de7104
dreamycurrentz <37789921+dreamycurrentz@users.noreply.github.com>
2024-10-13 19:10:14 +0530

    Update SerializeAndDeserialize_Practice.cs

--- SerializeAndDeserialize_Practice.cs
+++ SerializeAndDeserialize_Practice.cs
@@ -109,3 +109,25 @@ namespace XMLDemo
         }
     }
 }
+
+
+/*Member.cs */
+using System;
+using System.Xml.Serialization;
+
+namespace XMLDemo
+{
+    
+    public class Member
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+        
+        public DateTime JoiningDate { get; set; }
+
+        public bool IsPlatimumMember { get; set; }
+    }
+}
